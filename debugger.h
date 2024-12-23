57e3d2817cc9f9bd4082b603f27be45b7459e6e7
edwardMiller123 <edwardomiller123@gmail.com>
2024-12-23 12:21:15 +0000

    start on break points

--- debugger.h
+++ debugger.h
@@ -1,3 +1,18 @@
+#define MAX_BREAKPOINTS 64
+
+typedef enum BreakPointType {
+	LINE,
+	ADDR,
+} BreakPointType;
+
+// Information about the break point. Only the fields that correspond
+// the the appropriate type will be filled.
+typedef struct BreakPoint {
+	BreakPointType type;
+	int addr;
+	int line;
+} BreakPoint;
+
 typedef struct Debugger {
 	int pid;
 	int wait_status;
